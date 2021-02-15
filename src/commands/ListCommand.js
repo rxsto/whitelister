f649710f0748ca83c285cf3fbf07109d2dfc99d9
Oskar Lang <oskar.risto.lang@gmail.com>
2021-02-15 16:43:37 +0100

    feat: format whitelisted player list

--- src/commands/ListCommand.js
+++ src/commands/ListCommand.js
@@ -8,10 +8,10 @@ class RemoveCommand extends Command {
   }
 
   async execute (msg) {
-    const list = await this.client.whitelist.list()
+    const result = await this.client.whitelist.list()
+    const list = result.split('players: ')[1].split(', ')
     const embed = new MessageEmbed()
-      .setTitle(`All whitelisted players`)
-      .setDescription(list)
+      .addField({ name: `${process.env.EMOJI_INFO} Whitelisted Players`, value: list.map(user => `\`${user}\``).join(', ') })
       .setTimestamp(new Date())
       .setColor(0x0FABDD)
     msg.channel.send(embed)
