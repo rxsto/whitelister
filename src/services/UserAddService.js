9d77ba1263482683e8367a158d701b9ec4e43b56
Oskar Lang <oskar.risto.lang@gmail.com>
2021-02-15 16:05:43 +0100

    feat: add more detailed logging

--- src/services/UserAddService.js
+++ src/services/UserAddService.js
@@ -7,9 +7,11 @@ class UserAddService extends Service {
 
   add (users) {
     const executions = []
+    console.info(`Adding ${users.length === 1 ? 'user' : 'users'} ${users.join(', ')}...`)
     users.forEach(user => this.client.rcon.send(`whitelist add ${user.name}`))
     Promise.all(executions).then(results => {
       console.info(results.join('\n'))
+      console.info(`Successfully added ${users.length} ${users.length === 1 ? 'user' : 'users'}!`)
     }).catch(error => {
       console.error(error)
       throw new Error(error)
